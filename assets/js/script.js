bb6d9375479425723be2adc0d22c57c7346b82ff
Devin Winando <devin.winando13@gmail.com>
2022-01-30 20:38:28 +0700

    add: section FAQ

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+new Accordion([".accordion-container-1", ".accordion-container-2"], {
+  duration: 400,
+  triggerClass: "ac-header",
+  onOpen: function onOpen(currElement) {
+    const minus = document.querySelector("#" + currElement.id + " .ph-minus-bold");
+    const plus = document.querySelector("#" + currElement.id + " .ph-plus-bold");
+
+    plus.style.opacity = "0";
+    minus.style.opacity = "1";
+    plus.style.display = "none";
+    minus.style.display = "block";
+  },
+  onClose: function onClose(currElement) {
+    const minus = document.querySelector("#" + currElement.id + " .ph-minus-bold");
+    const plus = document.querySelector("#" + currElement.id + " .ph-plus-bold");
+
+    plus.style.opacity = "1";
+    minus.style.opacity = "0";
+    plus.style.display = "block";
+    minus.style.display = "none";
+  },
+});
